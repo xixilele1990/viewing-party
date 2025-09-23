cbc7de2260b4fcbea6061af081898807c96de06c
Gina Song <gina0song@gmail.com>
2025-09-23 00:06:15 -0700

    Update wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,31 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+    return {
+        "title":title,
+        "genre":genre,
+        "rating":rating
+    }
+    
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return  user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            break
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
